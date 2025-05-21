83af75bcd33eabedc0d725bf82c95e3344c4c36d
seongwon02 <seongwon2303@gmail.com>
2025-05-22 06:58:46 +0900

    lv6 완료

--- src/main/java/com/example/scheduler/lv6/dto/EventRequestDto.java
+++ src/main/java/com/example/scheduler/lv6/dto/EventRequestDto.java
@@ -1,15 +1,25 @@
 package com.example.scheduler.lv6.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class EventRequestDto {
 
-
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank
     private String password;
+
     private String title;
+
+    @NotBlank
+    @Size(max = 200)
     private String contents;
 
 }
